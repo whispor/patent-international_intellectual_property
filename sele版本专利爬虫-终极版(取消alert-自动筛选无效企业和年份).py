14d4c3b8d24cd4076893fe90af13d05929f457a8
whispor <36132293+whispor@users.noreply.github.com>
2019-09-01 16:43:12 +0800

    Add files via upload

--- sele版本专利爬虫-终极版(取消alert-自动筛选无效企业和年份).py
+++ sele版本专利爬虫-终极版(取消alert-自动筛选无效企业和年份).py
@@ -63,13 +63,7 @@ def data_exist_judge(browser,firm):
     time.sleep(get_random_wait(1, 2, 2))
     browser.execute_script("excuteTableSearch();")
     WebDriverWait(browser,10).until(EC.presence_of_element_located((By.XPATH,'//*[@id="page_top"]/div/div/div')))
-    # browser.find_element_by_css_selector("#tool_sort > a").click()
-    page_sort_1 = browser.find_element_by_css_selector("#tool_sort > a > span")
-    browser.execute_script("arguments[0].click();", page_sort_1)
-    time.sleep(get_random_wait(0.2, 1, 2))
-    page_sort_2 = browser.find_element_by_css_selector("#tool_sort > a > ul > li:nth-child(4) > a")
-    browser.execute_script("arguments[0].click();", page_sort_2)
-    time.sleep(get_random_wait(0.5, 1, 2))
+    time.sleep(get_random_wait(1.5, 2.5, 2))
     html = browser.page_source
     patent_info_all = re.findall(re_rule_1, html, re.S)
     if patent_info_all:
@@ -80,22 +74,27 @@ def data_exist_judge(browser,firm):
         patent_info_all = re.findall(re_rule_2, detail_content, re.S) 
         patent_info_all = [one.strip() for one in patent_info_all]
 
-    begin_date = browser.find_element_by_css_selector("#resultMode > div > div.list-container > ul > li:nth-child(1) > div > div.item-content.clear > div.item-content-body.left > p:nth-child(4) > a").text
-    begin_date = begin_date.split(".")
-    print(begin_date)
-    slices_date = date[0:date.index(int(begin_date[0]))]
-    print(slices_date)
-    print(patent_info_all)
     label = 0
     if int(patent_info_all[0]) <= 0:
-        return browser, label, slices_date, begin_date
+        return browser, label
     else:
+        page_sort_1 = browser.find_element_by_css_selector("#tool_sort > a > span")
+        browser.execute_script("arguments[0].click();", page_sort_1)
+        time.sleep(get_random_wait(0.2, 1, 2))
+        page_sort_2 = browser.find_element_by_css_selector("#tool_sort > a > ul > li:nth-child(4) > a")
+        browser.execute_script("arguments[0].click();", page_sort_2)
+        time.sleep(get_random_wait(0.2, 1.5, 2))
+        begin_date = browser.find_element_by_css_selector("#resultMode > div > div.list-container > ul > li:nth-child(1) > div > div.item-content.clear > div.item-content-body.left > p:nth-child(4) > a").text
+        begin_date = begin_date.split(".")
+        print(begin_date)
+        slices_date = date[0:date.index(int(begin_date[0]))]
         label = 1
         return browser, label, slices_date, begin_date
 
+    print(patent_info_all)
+
 def page_spider(firm,browser,begin_date):
     patent_data = []
-    print('>> %s <<———————>> 有 <<详细专利数据！'%firm)
     for year in date[date.index(int(begin_date[0])):]:
         print('>> 正在 <<爬取第 %s 个企业——第 %s 年数据！'%(firm_name_list.index(firm), year))
         patent_data_year = []
@@ -121,10 +120,8 @@ def page_spider(firm,browser,begin_date):
             browser.execute_script("excuteTableSearch();")
 
             WebDriverWait(browser,10).until(EC.presence_of_element_located((By.XPATH,'//*[@id="page_top"]/div/div/div')))
-            # browser.execute_script("document.querySelector("#resultlistForm_top")")
             time.sleep(get_random_wait(2, 3, 2))
             html = browser.page_source
-            # print(html)
             patent_info = re.findall(re_rule_1, html, re.S)
             if patent_info:
                 pass
@@ -166,115 +163,17 @@ if __name__ == '__main__':
 
     firm_name_list = get_firm_list(path_r)
     browser = log_in(url)
-    for firm in firm_name_list[551:800]:
+    for firm in firm_name_list[554:800]:
         print('-----------------------------------------------------------------------------------------------------------')
         print('>> 开始 <<爬取第 %s 个企业！'%firm_name_list.index(firm))
-        browser, label, slices_date, begin_date = data_exist_judge(browser,firm)
-        if label == 0:
+        browser_label_slices_date_begin_date = data_exist_judge(browser,firm)
+        if browser_label_slices_date_begin_date[1] == 0:
             patent_data = [[year, 0, 0, 0] for year in date]
             write_to_excel(firm, patent_data, path_w)
             print('>> %s <<———————>> 无 <<专利数据！'%firm)
-        elif label == 1:
+        elif browser_label_slices_date_begin_date[1] == 1:
             print('>> %s <<———————>> 有 <<详细专利数据！'%firm)
-            patent_data_null = [[year, 0, 0, 0] for year in slices_date]
+            patent_data_null = [[year, 0, 0, 0] for year in browser_label_slices_date_begin_date[2]]
             write_to_excel(firm, patent_data_null, path_w)
-            patent_data = page_spider(firm, browser, begin_date)
-
-        write_to_excel(firm, patent_data, path_w)
-
-
-
-
-
-# for firm in firm_name_list[547:800]:
-#     print('-----------------------------------------------------------------------------------------------------------')
-#     print('>> 开始 <<爬取第 %s 个企业！'%firm_name_list.index(firm))
-#     patent_data = []
-#     # trick = 0
-#     browser.execute_script("clearSearchExp();")
-#     test_formula = '申请（专利权）人=(%s)'%firm
-#     browser.find_element_by_css_selector("#searchExpDisplay").send_keys(test_formula)
-#     time.sleep(get_random_wait(1, 2, 2))
-#     browser.execute_script("excuteTableSearch();")
-#     WebDriverWait(browser,10).until(EC.presence_of_element_located((By.XPATH,'//*[@id="page_top"]/div/div/div')))
-#     # browser.find_element_by_css_selector("#tool_sort > a").click()
-#     page_sort_1 = browser.find_element_by_css_selector("#tool_sort > a > span")
-#     browser.execute_script("arguments[0].click();", page_sort_1)
-#     time.sleep(get_random_wait(0.2, 1, 2))
-#     page_sort_2 = browser.find_element_by_css_selector("#tool_sort > a > ul > li:nth-child(4) > a")
-#     browser.execute_script("arguments[0].click();", page_sort_2)
-#     time.sleep(get_random_wait(0.5, 1, 2))
-#     html = browser.page_source
-#     patent_info_all = re.findall(re_rule_1, html, re.S)
-#     if patent_info_all:
-#         pass
-#     else:
-#         content = browser.find_element_by_css_selector("div.page_top")
-#         detail_content = content.text
-#         patent_info_all = re.findall(re_rule_2, detail_content, re.S) 
-#         patent_info_all = [one.strip() for one in patent_info_all]
-
-#     begin_date = browser.find_element_by_css_selector("#resultMode > div > div.list-container > ul > li:nth-child(1) > div > div.item-content.clear > div.item-content-body.left > p:nth-child(4) > a").text
-#     begin_date = begin_date.split(".")
-#     print(begin_date)
-#     slices_date = date[0:date.index(int(begin_date[0]))]
-#     print(slices_date)
-#     print(patent_info_all)
-
-#     if int(patent_info_all[0]) <= 0:
-#         patent_data = [[year, 0, 0, 0] for year in date]
-#         write_to_excel(firm, patent_data, path_w)
-#         print('>> %s <<———————>> 无 <<专利数据！'%firm)
-#     else:
-#         print('>> %s <<———————>> 有 <<详细专利数据！'%firm)
-#         for year in date[date.index(int(begin_date[0])):]:
-#             print('>> 正在 <<爬取第 %s 个企业——第 %s 年数据！'%(firm_name_list.index(firm), year))
-#             patent_data_year = []
-#             patent_data_year.append(year)
-#             count = 0
-#             for patent_type in types:
-#                 try:
-#                     browser.find_element_by_css_selector("body > div.ui-popup.ui-popup-modal.ui-popup-show.ui-popup-focus")
-#                     browser.find_element_by_css_selector("body > div.ui-popup.ui-popup-modal.ui-popup-show.ui-popup-focus > div > table > tbody > tr:nth-child(3) > td > div.ui-dialog-button > button").click()
-#                     time.sleep(get_random_wait(0.5, 1.2, 2))
-#                     print("clear the alert!")
-#                 except:
-#                     pass
-
-#                 print('>> 正在 <<爬取第 %s 个企业——第 %s 年数据！'%(firm_name_list.index(firm), year))
-#                 search_formula = get_index_Formula(year, firm, patent_type)
-#                 print(search_formula)
-#                 browser.execute_script("clearSearchExp();")
-#                 # browser.find_element_by_css_selector("a.btn.btn-remove").click()
-#                 browser.find_element_by_css_selector("#searchExpDisplay").send_keys(search_formula)
-#                 time.sleep(get_random_wait(3, 4.5, 2))
-#                 # browser.find_element_by_css_selector("a.btn.btn-search").click()
-#                 browser.execute_script("excuteTableSearch();")
-
-#                 WebDriverWait(browser,10).until(EC.presence_of_element_located((By.XPATH,'//*[@id="page_top"]/div/div/div')))
-#                 # browser.execute_script("document.querySelector("#resultlistForm_top")")
-#                 time.sleep(get_random_wait(2, 3, 2))
-#                 html = browser.page_source
-#                 # print(html)
-#                 patent_info = re.findall(re_rule_1, html, re.S)
-#                 if patent_info:
-#                     pass
-#                 else:
-#                     content = browser.find_element_by_css_selector("div.page_top")
-#                     detail_content = content.text
-#                     patent_info = re.findall(re_rule_2, detail_content, re.S) 
-#                     patent_info = [one.strip() for one in patent_info]
-                    
-#                 print(patent_info)
-
-#                 if int(patent_info[0]) == count:
-#                     time.sleep(get_random_wait(1, 2, 2))
-#                 elif int(patent_info[0]) < 0:
-#                     patent_info.pop()
-#                     patent_info.append('0')
-#                 else:
-#                     pass
-#                 count = int(patent_info[0])
-#                 patent_data_year.append(count)
-#             patent_data.append(patent_data_year)
-#         write_to_excel(firm, patent_data, path_w)
+            patent_data = page_spider(firm, browser_label_slices_date_begin_date[0], browser_label_slices_date_begin_date[3])
+            write_to_excel(firm, patent_data, path_w)
